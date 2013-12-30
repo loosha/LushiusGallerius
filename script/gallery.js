b5a1ecf015a6c831223f5f2d4120d069c5158d3f
unknown <eyalz@eyalz8710.mckit.co.il>
2013-12-30 17:19:30 +0200

    initial commit

---
+++ script/gallery.js
@@ -0,0 +1,49 @@
+﻿// very good! :)
+// $(document).ready...
+
+//alert('yey! i was loaded!');
+// ok my language was hebrew.. oops
+
+$(document).ready(function(){
+
+    // the DOM is ready
+
+    // a button is clicked
+    $('.kaftor').click(function() {
+        // find the source of the image that was clicked
+        var imgSrc = $(this)
+            .find('img')
+            .attr('src');
+
+        // change main image
+        $('#main-image').css('background-image', 'url(' + imgSrc + ')');
+    });
+
+
+    // this is one way of doing it
+/*
+    $("#one").click(function() {
+       // $(".main").toggleClass()
+        $(".main").html("<p><img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/298184_10150444734705973_715739271_n.jpg' width='100%' height='100%'/></p>");
+    })
+    $("#two").click(function() {
+        $(".main").html("<p><img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/298184_10150444734705973_715739271_n.jpg' width='100%' height='100%'/></p>");
+    })
+    $("#three").click(function() {
+        $(".main").html("<p><img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/298184_10150444734705973_715739271_n.jpg' width='100%' height='100%'/></p>");
+    })
+    $("#four").click(function() {
+        $(".main").html("<p><img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/298184_10150444734705973_715739271_n.jpg' width='100%' height='100%'/></p>");
+    })
+    $("#five").click(function() {
+        $(".main").html("<p><img src='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/298184_10150444734705973_715739271_n.jpg' width='100%' height='100%'/></p>");
+    })
+*/
+
+        });
+
+/*
+var c = document.getElementById("#myCanvas");
+var ctx= c.getContext();
+ctx.font="30px Ariel";
+ctx.strokeText("Lusha's Site", 80, 80);*/
